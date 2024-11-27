544025486fd23d315d13334d36d5724a8d2e16af
thomas-god <thomas.godlewski@icloud.com>
2024-11-27 16:43:06 +0100

    feat: initial implementation for #ZU2

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,80 @@
 #![allow(unused_imports)]
 use std::{
-    io::{Read, Write},
-    net::TcpListener,
+    io::{ErrorKind, Read, Write},
+    net::{TcpListener, TcpStream},
 };
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    listener
+        .set_nonblocking(true)
+        .expect("Cannot put TCP listener in non-blocking mode");
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
-                let mut buf = [0u8; 128];
-                loop {
-                    match stream.read(&mut buf) {
-                        Ok(_) => {
-                            stream.write_all(b"+PONG\r\n");
-                            ()
-                        }
-                        Err(_) => {
-                            println!("Some error");
-                            ()
-                        }
-                    }
-                }
+    let mut tasks: Vec<PongTask> = Vec::new();
+
+    loop {
+        match listener.accept() {
+            Ok((stream, _)) => {
+                let task = PongTask::new(stream);
+                tasks.push(task);
             }
-            Err(e) => {
-                println!("error: {}", e);
+            Err(ref e) if e.kind() == ErrorKind::WouldBlock => {
+                // println!("No new connection");
+            }
+            Err(err) => {
+                println!("An error occured: {}", err);
+            }
+        }
+
+        for task in tasks.iter_mut() {
+            (*task).poll();
+        }
+
+        tasks.retain(|task| task.active);
+        // println!("End of main event loop");
+    }
+}
+
+struct PongTask {
+    stream: TcpStream,
+    buffer: [u8; 128],
+    active: bool,
+}
+
+impl PongTask {
+    fn new(stream: TcpStream) -> PongTask {
+        let buffer = [0u8; 128];
+        stream
+            .set_nonblocking(true)
+            .expect("Cannot put TCP stream in non-blocking mode");
+        PongTask {
+            stream,
+            buffer,
+            active: true,
+        }
+    }
+
+    fn poll(&mut self) {
+        match self.stream.read(&mut self.buffer) {
+            Ok(0) => {
+                println!("Stream terminated");
+                self.active = false;
+            }
+            Ok(_) => {
+                // println!("Received stream buffer: {:?}", &self.buffer);
+                match self.stream.write_all(b"+PONG\r\n") {
+                    Ok(_) => {}
+                    Err(_) => {}
+                };
+            }
+            Err(ref e) if e.kind() == ErrorKind::WouldBlock => {
+                // println!("No data in stream");
+            }
+            Err(err) => {
+                println!("Stream terminated with err: {}", err);
+                self.active = false;
             }
         }
     }
